c216fb7c5e693dc4f84969337fe72cb71f831809
Siam Talukder <siamtalukder484@gmail.com>
2026-09-14 12:00:25 +0600

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const apiRoutes = require("./api");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
 
 const api = process.env.BASE_URL;
 
 router.use(api, apiRoutes);
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.use(api, (req, res) =>
   res
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: 'localhost:8000'
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./routes/index.js'];
+
+
+swaggerAutogen(outputFile, routes, doc);
